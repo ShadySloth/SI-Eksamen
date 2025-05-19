dee3411a708c4a8eeafeb666b6361ef602a3eff2
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2025-05-19 15:54:50 +0200

    fix: mssing var

--- Backend-Service/Program.cs
+++ Backend-Service/Program.cs
@@ -38,6 +38,7 @@ if (File.Exists("../.env"))
 string username = Environment.GetEnvironmentVariable("POSTGRES_USER") ?? "";
 string password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD") ?? "";
 string database = Environment.GetEnvironmentVariable("POSTGRES_DATABASE") ?? "";
+string dbHost = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost";
 
 builder.Services.AddDbContext<ImageContext>(options =>
     options.UseNpgsql($"Host={dbHost};Database={database};Username={username};Password={password}"));
