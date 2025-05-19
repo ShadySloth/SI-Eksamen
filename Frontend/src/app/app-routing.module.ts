17e3ba656af52f6568d5cb1696dae21926025a40
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2025-05-19 16:46:19 +0200

    fix: fixed routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ImagePageComponent} from "./pages/image-page/image-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', redirectTo: '/image', pathMatch: 'full'},
+  {path : "image", component: ImagePageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
